47bd176a2263da69a1f9f9148c7f165ca70cdcb1
dugiwarc <gicubotnaru@gmail.com>
2019-02-09 20:41:47 +0100

    initial npm install

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(bodyParser.json());
 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/tusky", { useNewUrlParser: true });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
