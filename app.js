11947fa30a004d0dc5cdfd4ef42d8e25a74b7d31
dugiwarc <gicubotnaru@gmail.com>
2019-02-14 23:12:11 +0100

    add ACAO

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ var http = require('http');
  * Create HTTP server.
  */
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
