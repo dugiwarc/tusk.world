b625a52f979bd939a103ed01a82f9302c9eeec9e
dugiwarc <gicubotnaru@gmail.com>
2019-02-15 10:32:01 +0100

    bad commit

--- app.js
+++ app.js
@@ -31,13 +31,20 @@ var miscRoutes = require('./routes/misc');
 
 var app = express();
 app.use(cors());
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
     next();
-  });
+  }
+});
 var debug = require('debug')('tusk:server');
 var http = require('http');
 /**
@@ -55,7 +62,7 @@ var server = app.listen(port);
 var options = {
   allowUpgrades: true,
   transports: ['polling', 'websocket'],
-  pingTimeout: 9000,
+  pingTimeout: 9000, 
   pingInterval: 3000,
   cookie: 'mycookie',
   httpCompression: true,
@@ -244,7 +251,7 @@ var http = require('http');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT);
+var port = normalizePort(process.env.PORT || 3000);
 app.set('port', port);
 
 /**
